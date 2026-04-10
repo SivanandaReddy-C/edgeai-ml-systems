df8d0a7b69f4da2b2eb2eb899517c028f4cc8c71
C Sivananda Reddy <nanduksrmce@gmail.com>
2026-04-10 14:28:15 +0530

    Day 16: Profiled memory usage during ONNX inference

---
+++ phase2/onnx/profile_memory.py
@@ -0,0 +1,22 @@
+import onnxruntime as ort
+import numpy as np
+from memory_profiler import profile
+
+@profile
+def run_inference():
+    session = ort.InferenceSession(
+        "phase2/onnx/transformer.onnx",
+        providers=["CPUExecutionProvider"]
+    )
+
+    input_name = session.get_inputs()[0].name
+
+    input_data = np.random.randn(1, 28, 28).astype(np.float32)
+
+    for _ in range(100):
+        session.run(None, {input_name: input_data})
+
+if __name__=="__main__":
+    run_inference()
+
+    
